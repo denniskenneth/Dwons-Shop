70399ed40731584ae6d20fc327128b99546c77ea
Dennis Kenneth <dennyify123@gmail.com>
2021-10-23 09:56:10 +0000

    img added, styles created, login -- inprogress ....

---
+++ components/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { View, Text, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+// COLOR DEFINITIONS
+export const Colors = {
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1F2937',
+  darklight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
+  red: '#EF4444',
+};
+
+const StatusBarHeight = Constants.statusBarHeight;
+
+const { primary, secondary, tertiary, darklight, brand, green, red } = Colors;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 10}px;
+  background-color: ${primary};
+`;
+
+export const InnerContainer = styled.View`
+    flex: 1;
+    width: 100%
+    align-items: center;
+`;
+
+export const PageLogo = styled.Image`
+  width: 250px;
+  height: 200px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${tertiary};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react';
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+// IMPORT CUSTOM COMPONENTS
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+} from '../components/styles';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <StyledContainer>
+      <StatusBar style='dark' />
+      <InnerContainer>
+        <PageLogo
+          resizeMode='cover'
+          source={require('../assets/img/imgLogo.png')}
+        />
+        <PageTitle>Dwon's Shop</PageTitle>
+      </InnerContainer>
+    </StyledContainer>
   );
 };
 
